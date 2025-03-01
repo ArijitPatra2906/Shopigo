ca343df632c3906cd6b6fabaa9cd20b572e7423c
ArijitPatra2906 <patraarijit440@gmail.com>
2025-03-01 13:47:24 +0530

    untill product page

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,73 @@
+'use client'
+import { cn, formatCurrency } from '@/lib/utils'
+
+const ProductPrice = ({
+  sellingPrice,
+  className,
+  mrp = 0,
+  isDeal = false,
+  forListing = true,
+  plain = false,
+}: {
+  sellingPrice: number
+  isDeal?: boolean
+  mrp?: number
+  className?: string
+  forListing?: boolean
+  plain?: boolean
+}) => {
+  const discountPercent = Math.round(100 - (sellingPrice / mrp) * 100)
+  const stringValue = sellingPrice.toString()
+  const [intValue, floatValue] = stringValue.includes('.')
+    ? stringValue.split('.')
+    : [stringValue, '']
+
+  return plain ? (
+    formatCurrency(sellingPrice)
+  ) : mrp == 0 ? (
+    <div className={cn('text-3xl', className)}>
+      <span className='text-xs align-super'>₹</span>
+      {intValue}
+      <span className='text-xs align-super'>{floatValue}</span>
+    </div>
+  ) : isDeal ? (
+    <div className='space-y-2'>
+      <div
+        className={`flex ${forListing && 'justify-center'} items-center gap-2`}
+      >
+        <div className={cn('text-3xl', className)}>
+          <span className='text-xs align-super'>₹</span>
+          {intValue}
+          <span className='text-xs align-super'>{floatValue}</span>
+        </div>
+        <div className='flex justify-center items-center gap-2'>
+          <span className='bg-red-700 rounded-sm p-1 text-white text-sm font-semibold'>
+            {discountPercent}% Off
+          </span>
+          <span className='text-red-700 text-xs font-bold'>
+            Limited time deal
+          </span>
+        </div>
+      </div>
+      <div className='text-muted-foreground text-xs py-2'>
+        M.R.P: <span className='line-through'>{formatCurrency(mrp)}</span>
+      </div>
+    </div>
+  ) : (
+    <div className=''>
+      <div className='flex justify-center gap-3'>
+        <div className='text-3xl text-orange-700'>-{discountPercent}%</div>
+        <div className={cn('text-3xl', className)}>
+          <span className='text-xs align-super'>₹</span>
+          {intValue}
+          <span className='text-xs align-super'>{floatValue}</span>
+        </div>
+      </div>
+      <div className='text-muted-foreground text-xs py-2'>
+        M.R.P: <span className='line-through'>{formatCurrency(mrp)}</span>
+      </div>
+    </div>
+  )
+}
+
+export default ProductPrice
---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Amazon Clone'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less,enjoy more'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Shop with confidence, know what you are buying'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
