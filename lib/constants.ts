88db9e8dcc18e7898cbd30a8e9048d8102006940
ArijitPatra2906 <patraarijit440@gmail.com>
2025-03-01 15:34:53 +0530

    name change

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Amazon Clone'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Shopigo'
 export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less,enjoy more'
 export const APP_DESCRIPTION =
